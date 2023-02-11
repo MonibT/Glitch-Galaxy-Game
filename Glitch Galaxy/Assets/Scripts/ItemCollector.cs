03e5402d5dc15c7e02e897abe1cc85240e829ec5
monibtaha <monibtaha81@gmail.com>
2023-02-11 11:31:56 +0200

    Glitch Galaxy main

---
+++ Glitch Galaxy/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int Pineapples = 0;
+    public Text PineapplesText;
+    public AudioSource collectsound;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Pineapple"))
+        {
+            Destroy(collision.gameObject);
+            Pineapples++;
+            collectsound.Play();
+            PineapplesText.text = "Pineapples: " + Pineapples;
+
+        }
+    }
+
+    
+}
---
+++ Glitch Galaxy/Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    public AudioSource deathsound;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+ 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Traps"))
+        {
+            deathsound.Play();
+            Die();
+        }
+    }
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+        
+    }
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
